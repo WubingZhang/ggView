5c4ea6386b839d6f31fd399dc74d0686caa003c3
Wubing Zhang <watson5bzhang@gmail.com>
2020-11-07 15:42:00 -0500

    customize color and fill in ggplot instead of geom_box

--- R/BoxView.R
+++ R/BoxView.R
@@ -48,40 +48,35 @@ BoxView <- function(gg, x, y,
                     legend.position = "none",
                     ...){
   gg = as.data.frame(gg)
-  p = ggplot(gg, aes_string(x, y))
   ## Check if color is valid color
   boo <- try(col2rgb(color), silent=TRUE)
   boo1 = "try-error" %in% class(boo)
   boo <- try(col2rgb(fill), silent=TRUE)
   boo2 = "try-error" %in% class(boo)
+
   if(color %in% colnames(gg)){ ## Customize colors in the data
     if(fill %in% colnames(gg))
-      p = p + geom_boxplot(aes_string(color = color, fill = fill),
-                           width = width, size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y, color = color, fill = fill))
     else if(!boo2)
-      p = p + geom_boxplot(aes_string(color = color), fill = fill,
-                           width = width, size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y, color = color), fill = fill)
     else
-      p = p + geom_boxplot(aes_string(color = color), width = width,
-                           size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y, color = color))
   }else if(!boo1){ ## Customize single color
     if(fill %in% colnames(gg))
-      p = p + geom_boxplot(aes_string(fill = fill), color = color,
-                           width = width, size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y, fill = fill), color = color)
     else if(!boo2)
-      p = p + geom_boxplot(color = color, fill = fill, width = width,
-                           size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y), fill = fill, color = color)
     else
-      p = p + geom_boxplot(color = color, width = width, size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y), color = color)
   }else{ ## Use default color
     if(fill %in% colnames(gg))
-      p = p + geom_boxplot(aes_string(fill = fill), width = width,
-                           size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y, fill = fill))
     else if(!boo2)
-      p = p + geom_boxplot(fill = fill, width = width, size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y), fill = fill)
     else
-      p = p + geom_boxplot(width = width, size = size, alpha = alpha, ...)
+      p = ggplot(gg, aes_string(x, y))
   }
+  p = p + geom_boxplot(width = width, size = size, alpha = alpha, ...)
   ## Comparisons
   if(!is.null(comparisons)){
     p = p + ggpubr::compare_means(method = test.method, p.adjust.method = "fdr")
@@ -96,7 +91,7 @@ BoxView <- function(gg, x, y,
     else
       p = p + geom_jitter(size = jitter.size)
   }
-  p = p + labs(x = xlab, y = ylab, title = main)
+  p = p + labs(x = xlab, y = ylab, title = main, color = NULL, fill = NULL)
   p = p + theme_bw(base_size = 12)
   p = p + theme(plot.title = element_text(hjust = 0.5),
                 legend.position = legend.position)
