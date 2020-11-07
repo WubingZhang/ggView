2f9085f0d2a353383b0397e7255a42823ca95932
Wubing Zhang <watson5bzhang@gmail.com>
2020-11-07 16:40:05 -0500

    allow adding jitters and labels in the plot

--- R/BoxView.R
+++ R/BoxView.R
@@ -11,14 +11,24 @@
 #' @param fill Character, specifying the column name/fill color.
 #' @param width Numeric, specifying the box width.
 #' @param size Numeric, specifying size of the box.
+#'
 #' @param comparisons A list, customizing the comparisons.
 #' @param test.method Character, specifying the statistical test method.
 #' @param p.label Character, "p.signif" (shows the significance levels), or
 #' "p.format" (shows the formatted p value).
+#'
 #' @param add.jitter Boolean, whether add jitter into the plot.
 #' @param jitter.color Character, specifying the column name/color of the points.
 #' @param jitter.size Numeric, specifying size of the jitter.
-#' @param alpha A numeric, specifying the transparency of the dots.
+#' @param jitter.shape Numeric, specifying the shape of the dots.
+#' @param jitter.width Numeric, specifying the width of the jitter region.
+#'
+#' @param label Character, specifying the column name for dot label.
+#' @param toplabels Character, specifying the column name/color of the points.
+#' @param label.size Numeric, specifying size of the labels.
+#' @param label.force Numeric, force of repulsion between overlapping text labels. Defaults to 3.
+#'
+#' @param alpha A numeric, specifying the transparency of the boxes.
 #' @param main Title of the figure.
 #' @param xlab Title of x-axis
 #' @param ylab Title of y-axis.
@@ -34,17 +44,23 @@
 #' @export
 
 BoxView <- function(gg, x, y,
-                    color = NA,
+                    color = "black",
                     fill = NA,
-                    width = 0.6,
+                    width = 0.5,
                     size = 1,
                     comparisons = NULL,
                     test.method = "t.test",
                     p.label = c("p.signif", "p.format")[1],
                     add.jitter = FALSE,
-                    jitter.color = color,
+                    jitter.color = "black",
                     jitter.size = size,
-                    alpha = 0.6,
+                    jitter.shape = 16,
+                    jitter.width = 0.2,
+                    label = NULL,
+                    toplabels = c(),
+                    label.size = 5,
+                    label.force = 3,
+                    alpha = 1,
                     main = NULL,
                     xlab = NULL,
                     ylab = y,
@@ -56,6 +72,7 @@ BoxView <- function(gg, x, y,
   boo1 = "try-error" %in% class(boo)
   boo <- try(col2rgb(fill), silent=TRUE)
   boo2 = "try-error" %in% class(boo)
+  if(is.na(fill)) boo2 = TRUE
 
   if(color %in% colnames(gg)){ ## Customize colors in the data
     if(fill %in% colnames(gg))
@@ -80,6 +97,7 @@ BoxView <- function(gg, x, y,
       p = ggplot(gg, aes_string(x, y))
   }
   p = p + geom_boxplot(width = width, size = size, alpha = alpha, ...)
+
   ## Comparisons
   if(!is.null(comparisons)){
     p = p + ggpubr::stat_compare_means(comparisons = comparisons,
@@ -87,18 +105,34 @@ BoxView <- function(gg, x, y,
                                        hide.ns = TRUE,
                                        label = p.label)
   }
+
+  ## Add dots in the plot
   if(add.jitter){
     boo <- try(col2rgb(jitter.color), silent=TRUE)
     boo3 = "try-error" %in% class(boo)
     if(jitter.color %in% colnames(gg))
-      p = p + geom_jitter(aes_string(color = jitter.color), size = jitter.size)
+      p = p + geom_jitter(aes_string(color = jitter.color),
+                          position=position_jitter(jitter.width),
+                          shape=jitter.shape, size = jitter.size)
     else if(!boo3)
-      p = p + geom_jitter(color = jitter.color, size = jitter.size)
+      p = p + geom_jitter(color = jitter.color,
+                          position=position_jitter(jitter.width),
+                          shape=jitter.shape, size = jitter.size)
     else
-      p = p + geom_jitter(size = jitter.size)
+      p = p + geom_jitter(position=position_jitter(jitter.width),
+                          shape=jitter.shape, size = jitter.size)
   }
+  if(!is.null(label) && label%in%colnames(gg)){
+    colnames(gg)[colnames(gg)==label] = "label"
+    gg$label[!gg$label%in%toplabels] = ""
+    p = p + ggrepel::geom_text_repel(data = gg,
+                                     aes(label = label),
+                                     size = label.size,
+                                     force = label.force)
+  }
+
   p = p + labs(x = xlab, y = ylab, title = main, color = NULL, fill = NULL)
-  p = p + theme_bw(base_size = 12)
+  p = p + theme_bw(base_size = 14)
   p = p + theme(plot.title = element_text(hjust = 0.5),
                 legend.position = legend.position)
   return(p)
