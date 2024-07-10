3750fc3a9f717b04349e3a71524febeec393a0ef
Wubing Zhang <31879597+WubingZhang@users.noreply.github.com>
2024-07-10 12:25:43 -0700

    Remove interaction and optimal cut from the arguments.

--- R/KMView.R
+++ R/KMView.R
@@ -5,11 +5,9 @@
 #' @param os A character or an integer, specifying the variable/column of overall survival.
 #' @param event A character or an integer, specifying the variable/column of outcome.
 #' @param adjust A vector of factors to include in the cox model, default c("age", "gender", "race").
-#' @param interact A factor that interact with `bio`.
 #' @param cut A numeric vector for separating the patients into different groups,
 #' only available when `bio` is a continuous variable.
 #' @param labels A character vector, specifying the labels for each patient group.
-#' @param optimalCut Boolean, indicating whether determin the ootimal cutpoint.
 #' @param pval.method  logrank (default) or cox, indicating the method for the pvalue calculation.
 #' @param pval.pos Character, indicating the method for the pvalue calculation.
 #' @param ... Other available parameters in ggsurvplot or ggadjustedcurves.
@@ -21,9 +19,8 @@
 #' @import survival survminer
 #' @export
 KMView <- function(survdata, bio, os = "os", event = "event",
-                   adjust = c("age", "gender", "race"), interact = NULL,
+                   adjust = c("age", "gender", "race"),
                    cut = c(0, 0.5, 1), labels = NULL,
-                   optimalCut = TRUE,
                    pval.method = c("cox", "logrank"),
                    pval.pos = c(0, 0.2),
                    ...){
@@ -42,39 +39,10 @@ KMView <- function(survdata, bio, os = "os", event = "event",
   colnames(tmpDat)[1:2] = c("os", "event")
 
   if(is.numeric(tmpDat[1, bio])){
-    ## Add interaction term into the tmpDat
-    if(length(interact)!=0 && interact[1]%in%colnames(tmpDat)){
-      tmpDat$Interaction = tmpDat[,bio] * tmpDat[,interact[1]]
-      tmpDat$Partner = tmpDat[,interact[1]]
-    }
     interestTerm = bio
-    if("Interaction" %in% colnames(tmpDat)) interestTerm = "Interaction"
+    if(length(unique(tmpDat[, bio]))>3) tmpDat[, bio] = scale(tmpDat[, bio])
     cox <- coxph(Surv(time=os, event=event) ~ ., data=tmpDat)
-    ## Select the cut points
-    if(optimalCut | is.null(cut)){
-      if(any(adjust%in%colnames(tmpDat))|"Interaction"%in%colnames(tmpDat)){
-        ## Select the best cut point based on the log-rank test.
-        perm_pvals = lapply(seq(0.1, 0.9, length.out = 100), function(x){
-          tmpCut = quantile(tmpDat[,interestTerm], x)
-          tmpDat[, interestTerm][tmpDat[, interestTerm]<tmpCut] = "low"
-          tmpDat[, interestTerm][tmpDat[, interestTerm]!="low"] = "high"
-          tmpDat[, interestTerm] = factor(tmpDat[, interestTerm],
-                                          levels = c("low", "high"))
-          errflag = FALSE
-          diffSurv <- tryCatch(survdiff(Surv(time=os, event=event) ~ ., data=tmpDat),
-                                 error = function(e) errflag <<- TRUE)
-          if(errflag) return(1)
-          pval <- signif(1 - pchisq(diffSurv$chisq, length(diffSurv$n) - 1), digits=3)
-        })
-        cut = c(0, seq(0.1, 0.9, length.out = 100)[which.min(unlist(perm_pvals))], 1)
-        breaks = quantile(tmpDat[,interestTerm], cut)
-      }else{
-        tmp <- surv_cutpoint(tmpDat, time = "os", event="event", variables = interestTerm)
-        breaks = c(min(tmpDat[,interestTerm]), tmp$cutpoint$cutpoint, max(tmpDat[,interestTerm]))
-      }
-    }else{
-      breaks = quantile(tmpDat[,interestTerm], cut)
-    }
+    breaks = quantile(tmpDat[,interestTerm], cut)
     ## Generate labels automatically
     if(is.null(labels)) labels = paste0("level", 1:(length(breaks)-1))
     labels = factor(labels, levels = labels)
@@ -93,13 +61,7 @@ KMView <- function(survdata, bio, os = "os", event = "event",
     if(is.null(labels)) labels = unique(tmpDat[, bio])
     names(labels) = unique(tmpDat[, bio])
     tmpDat[, bio] = as.factor(tmpDat[, bio])
-    ## Add interaction term into the tmpDat
-    if(length(interact)!=0 && interact[1]%in%colnames(tmpDat)){
-      tmpDat$Partner = as.factor(tmpDat[,interact[1]])
-      tmpDat$Interaction = as.numeric(tmpDat[,bio]) * as.numeric(tmpDat$Partner)
-    }
     interestTerm = bio
-    if("Interaction" %in% colnames(tmpDat)) interestTerm = "Interaction"
 
     ## Calculate the p-value for plotting
     cox <- coxph(Surv(time=os, event=event) ~ ., data=tmpDat)
@@ -110,12 +72,11 @@ KMView <- function(survdata, bio, os = "os", event = "event",
     hr.low <- round(tmpres$conf.int[idx,3], 3)
     hr.high <- round(tmpres$conf.int[idx,4], 3)
   }
+              
   ## Calculate the p-value for plotting
   if(pval.method[1]=="logrank"){
     if(any(adjust%in%colnames(tmpDat))){
       warning("Coxph p-value is used because of the adjustment factors in the analysis!!!")
-    }else if("Interaction"%in%colnames(tmpDat)){
-      warning("Coxph p-value is used because of the interaction term in the analysis!!!")
     }else{
       errflag = FALSE
       diffSurv <- tryCatch(survdiff(Surv(time=os, event=event) ~ ., data=tmpDat),
@@ -131,7 +92,7 @@ KMView <- function(survdata, bio, os = "os", event = "event",
   }
 
   ## Plotting the KM-plot
-  if(any(adjust%in%colnames(tmpDat)) | "Interaction"%in%colnames(tmpDat)){
+  if(any(adjust%in%colnames(tmpDat))){
     newcox = coxph(Surv(time=os, event=event) ~ ., data=tmpDat)
     p = ggadjustedcurves(newcox, variable = interestTerm, data = tmpDat,
                          legend.labs = labels, ...)
